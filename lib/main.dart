7a6fa4124e6b4ababde5693336be978b193a6580
Ameka <edetatj@gmail.com>
2024-06-03 22:23:33 +0000

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:ecowaste/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(apiKey:  "AIzaSyCAsrVpw981ZYnfZSBRTbm7euY6wmLcBSs", 
+      appId: "1:523222427904:web:1620fbd0a7740254082be5", 
+      messagingSenderId:  "523222427904", 
+      projectId:  "wasteapp-1fb69")
+    );
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import flutter_inappwebview_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
 }
